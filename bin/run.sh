ca8d425577048f8194577ea9f041b0f4b9692d2b
Simon Erhardt <me@rootlogin.ch>
2016-09-06 21:59:31 +0200

    switched to dropbear and improved

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $GENERATE_HOST_KEYS == 'YES' ]; then
-  ssh-keygen -A
+CONF_DIR="/etc/dropbear"
+SSH_KEY_DSS="${CONF_DIR}/dropbear_dss_host_key"
+SSH_KEY_RSA="${CONF_DIR}/dropbear_rsa_host_key"
+SSH_KEY_ECDSA="${CONF_DIR}/dropbear_ecdsa_host_key"
+
+# Check if conf dir exists
+if [ ! -d ${CONF_DIR} ]; then
+    mkdir -p ${CONF_DIR}
 fi
+chown root:root ${CONF_DIR}
+chmod 755 ${CONF_DIR}
 
-sshd
+# Check if keys exists
+if [ ! -f ${SSH_KEY_DSS} ]; then
+    dropbearkey -t dss -f ${SSH_KEY_DSS}
+fi
+chown root:root ${SSH_KEY_DSS}
+chmod 600 ${SSH_KEY_DSS}
+
+if [ ! -f ${SSH_KEY_RSA} ]; then
+    dropbearkey -t rsa -f ${SSH_KEY_RSA} -s 4096
+fi
+chown root:root ${SSH_KEY_RSA}
+chmod 600 ${SSH_KEY_RSA}
+
+if [ ! -f ${SSH_KEY_ECDSA} ]; then
+    dropbearkey -t ecdsa -f ${SSH_KEY_ECDSA} -s 521
+fi
+chown root:root ${SSH_KEY_ECDSA}
+chmod 600 ${SSH_KEY_ECDSA}
+
+exec /usr/sbin/dropbear -g -s -w -j -E -F
